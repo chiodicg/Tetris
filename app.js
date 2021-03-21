ee5b0c4963ead91db4279d2d68486346deac2d3b
Carla G. Chiodi <carla.chiodi@gmail.com>
2021-03-21 23:21:06 +0000

    Styling grid and setting variables

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //code here
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+  const score = document.querySelector('#score')
+  const startButton = document.querySelector('#start-pause')
+  console.log(squares)
 })
